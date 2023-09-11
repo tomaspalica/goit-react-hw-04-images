c0b0c98b76ed09f9947b9db008e6ab891255df54
tomaspalica <tomasss.palica@gmail.com>
2023-09-11 20:12:23 +0200

    Update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,10 @@ import { useEffect } from "react";
 import css from '../css/Modal.module.css'
 export const Modal= ({modalAlt,modalSrc, handleClose}) => {
     useEffect(() => {
+        const handleKeyDown = evt => {
+            if (evt.keyCode === 27) {
+               handleClose()}
+        }
         window.addEventListener('keydown', handleKeyDown);
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
@@ -9,10 +13,7 @@ export const Modal= ({modalAlt,modalSrc, handleClose}) => {
     }, [])
    
     
-const handleKeyDown = evt => {
-    if (evt.keyCode === 27) {
-       handleClose()}
-}
+
 
 
     return (
